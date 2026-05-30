18fdaa38f422bbd795a6654fd15d302c25e11809
dungxtd <dellsacben@gmail.com>
2026-05-30 19:42:41 +0700

    chore(nextflix): sha256 for v1.0.2

--- Casks/nextflix.rb
+++ Casks/nextflix.rb
@@ -1,6 +1,6 @@
 cask "nextflix" do
   version "1.0.2"
-  sha256 "REPLACE_WITH_DMG_SHA256_AFTER_FIRST_RELEASE"
+  sha256 "98c3429b66a248c7ebace2593924362f033952fd9f3e73972ad06211cece19f4"
 
   url "https://github.com/dungxtd/homebrew-tap/releases/download/nextflix-v#{version}/Nextflix_macos-arm64_Nextflix_#{version}_aarch64.dmg",
       verified: "github.com/dungxtd/homebrew-tap/"
